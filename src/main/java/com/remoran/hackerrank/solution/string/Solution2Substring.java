63da5f7074db9fd0fb2a9732c8f58e7a5972a17c
remoran <remorandev@gmail.com>
2019-04-26 14:19:25 +0200

    -ADD Solucion problema String  2 - Substring

---
+++ src/main/java/com/remoran/hackerrank/solution/string/Solution2Substring.java
@@ -0,0 +1,12 @@
+package com.remoran.hackerrank.solution.string;
+
+public class Solution2Substring {
+    public static void main(String[] args) {
+        String S = "Helloworld";
+        int start = 3;
+        int end = 7;
+
+        System.out.println(S.substring(start, end));
+
+    }
+}
